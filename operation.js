2bf5ebf630623fbf9cc757440230ce21784c2436
Kshitunharina <kshitunharina@gmail.com>
2025-10-14 17:38:59 +0530

    js first class

---
+++ operation.js
@@ -0,0 +1,21 @@
+console.log(9+10);
+
+var a = 12;
+console.log(a);
+// let b = 13
+// const c= 14
+
+var greetMe = "good morning"
+var collection = ["Bienve,Saavi","Enrico"]
+var n = true
+
+// +,-,*,/,%
+console.log(2%2)
+console.log(2/2);
+
+var i = 5
+ i += 2 //i = i+2 = 5+2 
+ console.log(i)
+ i-=0 = i=i-0
+ i*=4 = i=i*4
+ i/=1 = i=i/1
