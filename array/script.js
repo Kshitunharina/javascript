9902b0aefc7c1ed755ab7bd41e7799b5fd487a56
Kshitunharina <kshitunharina@gmail.com>
2025-10-31 16:59:38 +0530

    dom

--- array/script.js
+++ array/script.js
@@ -1,9 +1,27 @@
-var fruits = ["Mango","apple","banana","strawberry"]
-console.log(fruits[0])
 
-console.log(fruits.join("*"))
-console.log(fruits.pop())
-console.log(fruits)
-fruits.push("guava")
-console.log(fruits)
+// var vegetables = ["Carrot", "Broccoli", "Onion", "Potato"]
+// console.log(vegetables[0])
 
+// console.log(vegetables.join("; "))
+// console.log(vegetables.pop())
+// console.log(vegetables)
+
+// vegetables.push("Spinach")
+// console.log(vegetables)
+
+// function add(a,b) {
+//     return a+b;
+// }
+// function average(a,b) {
+//     return add(a,b)/2;
+// }
+// let result = average(97,47)
+// console.log(result)
+var todolist = ["Buy Groceries", "Walk the Dog", "Finish Homework"];
+
+console.log(todolist[0])
+
+todolist.push("Call mom", "Clean Room")
+console.log("After adding: " + todolist.join(", "));
+todolist.unshift("Pay Your Debt");
+console.log("Final list: " + todolist.join(", "));
---
+++ dom/script.js
@@ -0,0 +1,7 @@
+document.getElementById("firstparagraph").innerHTML="hello student"
+function myFunction(){
+    var a=4;
+    document.getElementById("buttoncontent").innerHTML=a*a
+    
+
+}
